86d20d0bb88299d57c459107aae5ffdc75ba64af
Theertha-R <theertharajil11a@gmail.com>
2024-10-13 11:00:05 +0530

    index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const dob = document.getElementById('dob').value;
-        const termsAccepted = document.getElementById('terms').checked ? 'Yes' : 'No';
+        const termsAccepted = document.getElementById('terms').checked ? 'true' : 'false';
         const entry = {
             name,
             email,
